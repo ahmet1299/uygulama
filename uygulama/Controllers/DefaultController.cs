c22d378328c916e8e97563b614afb373be7a2bd5
ycontarli <ycontarli@YCONTARLID54>
2018-07-31 11:57:35 +0300

    Add project files.

---
+++ uygulama/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using uygulama.Models;
+
+namespace uygulama.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            List<sinif> sinif_list = new List<sinif>()
+            {
+                new sinif {Id=1,username="ferhat" },
+                new sinif {Id=2, username="ahmet"}
+            };
+
+            return View(sinif_list);
+        }
+    }
+}
---
+++ uygulama/Models/sinif.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace uygulama.Models
+{
+    public class sinif
+    {
+        public int Id { get; set; }
+        public string username { get; set; }
+    }
+}
